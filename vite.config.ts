5311632efeb6c66c22ce0e01dc48bcbedf9f530b
Michell Gutierrez <michellgutierrez@hotmail.com>
2025-01-28 17:21:43 -0500

    fix netlify redirects

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,6 @@ import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 
 export default defineConfig({
-  publicDir: 'public', // Carpeta pública para copiar recursos estáticos
-  build: {
-    outDir: 'dist', // Carpeta de salida
-  },
   plugins: [react()],
   resolve: {
     alias: {
